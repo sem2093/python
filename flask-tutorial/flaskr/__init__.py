48c02a52db1290ae3713dee74938309ff500b5c3
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-31 10:27:53 -0600

    Update __init__.py

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -3,7 +3,8 @@ import os
 from flask import Flask
 from . import db
     db.init_app(app)
-
+from . import auth
+    app.register_blueprint(auth.bp)
 
 def create_app(test_config=None):
     # create and configure the app
